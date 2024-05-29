2e52d68a65ac48242c72371a9e7a6b68f97ac6fd
kk908676 <kk908676@gmail.com>
2024-05-29 16:11:31 +0800

    First submit

---
+++ Debug/FreeRTOS/portable/ARM_CM4F/port.d
@@ -0,0 +1,19 @@
+FreeRTOS/portable/ARM_CM4F/port.o: ../FreeRTOS/portable/ARM_CM4F/port.c \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/projdefs.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/portable.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/task.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/list.h
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/FreeRTOS.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/projdefs.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/portable.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/task.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/list.h:
---
+++ Debug/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+FreeRTOS/tasks.o: ../FreeRTOS/tasks.c \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/projdefs.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/portable.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/task.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/list.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/timers.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/task.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/stack_macros.h
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/FreeRTOS.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/projdefs.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/portable.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/task.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/list.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/timers.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/task.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/lab3/lab3/FreeRTOS/include/stack_macros.h:
